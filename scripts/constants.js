93a5b7d48839ae67caff3bffe14a462fcc581e7e
Amogh Bihani <amogh@amogh.(none)>
2014-12-28 00:52:25 +0530

    Removing hardcoding and UI update

---
+++ scripts/constants.js
@@ -0,0 +1,16 @@
+/**
+ * Project: Fun With Flags
+ * Author: Amogh Bihani
+ */
+
+var GAME_TYPE_NONE = -1;
+var GAME_TYPE_COUNTRY_TO_FLAG = 0;
+var GAME_TYPE_FLAG_TO_COUNTRY = 1;
+
+var GAME_MODE_NONE = -1;
+var GAME_MODE_ARCADE = 0;
+var GAME_MODE_QUIZ = 1;
+
+var NUM_QUESTIONS_IN_QUIZ = 10;
+var NUM_OPTIONS = 4;
+var NUM_DO_NOT_REPEAT_TILL = 50;
